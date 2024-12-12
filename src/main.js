d85f3cd02a83fff296d064d061bfeeeca2ca1ba8
HANSOL <hansol302@gmail.com>
2024-12-12 11:03:40 +0900

    Fix: 라이브러리 상대 경로로 변경

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "@/node_modules/three/build/three.module.js";
-import * as LocAR from "@/node_modules/locar/dist/locar.es.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
+import * as LocAR from "../node_modules/locar/dist/locar.es.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
