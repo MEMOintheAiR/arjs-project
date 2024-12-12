b8cd88cbb877318fac2c4eb97d7aed8a29ffee7f
HANSOL <hansol302@gmail.com>
2024-12-12 16:08:23 +0900

    Fix: 라이브러리 경로 수정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import * as THREE from "three";
-import * as LocAR from "locar";
+import * as THREE from "../node_modules/three/build/three.module";
+import * as LocAR from "../node_modules/locar/dist/locar.umd";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
